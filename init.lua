ee06ae0241d242df623a0c66c12557b6b4d5ad59
AndrijaSkontra <askontra22@student.unizd.hr>
2024-05-20 17:53:11 +0200

    transparent background

--- init.lua
+++ init.lua
@@ -680,6 +680,8 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
