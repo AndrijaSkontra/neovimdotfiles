908d60ca75e00f3d274aeb20d991ed964b64dae6
AndrijaSkontra <askontra22@student.unizd.hr>
2024-06-13 22:33:12 +0200

    add refactor -- leader re

--- init.lua
+++ init.lua
@@ -788,6 +788,7 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.comment',
   require 'kickstart.plugins.toggleterm',
+  require 'kickstart.plugins.refactor',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
@@ -889,3 +890,14 @@ end
 
 -- Bind the function to <leader>ee
 vim.api.nvim_set_keymap('n', '<leader>kk', ':lua toggle_copilot()<CR>', { noremap = true, silent = true })
+vim.keymap.set('x', '<leader>re', ':Refactor extract ')
+vim.keymap.set('x', '<leader>rf', ':Refactor extract_to_file ')
+
+vim.keymap.set('x', '<leader>rv', ':Refactor extract_var ')
+
+vim.keymap.set({ 'n', 'x' }, '<leader>ri', ':Refactor inline_var')
+
+vim.keymap.set('n', '<leader>rI', ':Refactor inline_func')
+
+vim.keymap.set('n', '<leader>rb', ':Refactor extract_block')
+vim.keymap.set('n', '<leader>rbf', ':Refactor extract_block_to_file')
